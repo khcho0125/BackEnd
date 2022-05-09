e37c52a2230ac8918acb676b63a3632d10807f35
cho kyeong hyun <khcho0125@icloud.com>
2022-05-09 11:28:57 +0900

    ♻️ :: Listener 분리

--- firstSocketio/src/main/java/com/firstsocketio/global/socketio/SocketIoService.java
+++ firstSocketio/src/main/java/com/firstsocketio/global/socketio/SocketIoService.java
@@ -2,6 +2,8 @@ package com.firstsocketio.global.socketio;
 
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,23 +21,24 @@ public class SocketIoService {
     public static final Map<String, SocketIOClient> connectMap = new ConcurrentHashMap<>();
 
     private final SocketIOServer socketIOServer;
+    private final ConnectListener connectListener = client -> {
+        String username = client.getHandshakeData().getSingleUrlParam("username");
+        log.info("{}, {}", username, client.getSessionId());
+        connectMap.put(username, client);
+        client.set("username", username);
+        log.info("Socket Connect User : {}", username);
+    };
+    private final DisconnectListener disconnectListener = client -> {
+        String username = client.getHandshakeData().getSingleUrlParam("username");
+        connectMap.remove(username);
+        client.disconnect();
+        log.info("Socket DisConnect User : {}", username);
+    };
 
     @PostConstruct
     private void start() {
-        socketIOServer.addConnectListener(client -> {
-            String username = client.getHandshakeData().getSingleUrlParam("username");
-            log.info("{}, {}", username, client.getSessionId());
-            connectMap.put(username, client);
-            client.set("username", username);
-            log.info("Socket Connect User : {}", username);
-        });
-        socketIOServer.addDisconnectListener(client -> {
-            String username = client.getHandshakeData().getSingleUrlParam("username");
-            connectMap.remove(username);
-            client.disconnect();
-            log.info("Socket DisConnect User : {}", username);
-        });
-
+        socketIOServer.addConnectListener(connectListener);
+        socketIOServer.addDisconnectListener(disconnectListener);
         socketIOServer.start();
     }
 
