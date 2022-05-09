4aa481e1657234d3ccdc88f7526ed291e0eedafb
cho kyeong hyun <khcho0125@icloud.com>
2022-05-09 11:27:41 +0900

    ⛏️ :: Send Message 추가

--- firstSocketio/src/main/java/com/firstsocketio/domain/ChatController.java
+++ firstSocketio/src/main/java/com/firstsocketio/domain/ChatController.java
@@ -2,23 +2,38 @@ package com.firstsocketio.domain;
 
 import com.corundumstudio.socketio.SocketIONamespace;
 import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.DataListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class ChatController {
 
-    private SocketIONamespace namespace;
+    private final SocketIONamespace namespace;
+    private final MessageRepository messageRepository;
 
-    public ChatController(SocketIOServer server) {
+    @Autowired
+    public ChatController(SocketIOServer server, MessageRepository messageRepository) {
         namespace = server.addNamespace("/chat");
-        namespace.addEventListener("send", ChatMessage.class, ((client, data, ackSender) -> {
-            client.sendEvent("getMessage", data);
-        }));
+        namespace.addEventListener("send", ChatMessage.class, onMessage());
+
+        this.messageRepository = messageRepository;
     }
 
     @GetMapping("/")
     public String index() {
         return "index";
     }
+
+    private DataListener<ChatMessage> onMessage() {
+        return (client, data, ackSender) -> {
+            log.info("message: {}\nuser: {}\nsessionId: {}", data.getMessage(), data.getUsername(), data.getSessionId());
+            namespace.getBroadcastOperations().sendEvent("newMessage", data);
+            messageRepository.save(data);
+        };
+    }
+
 }
