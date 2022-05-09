a0cc0e0089f702d9066c711248d2384c9b28b626
cho kyeong hyun <khcho0125@icloud.com>
2022-05-09 23:50:46 +0900

    ⛏️ :: Room 추가

--- Netty_Socket_Io_Server/src/main/java/com/firstsocketio/domain/MessageRepository.java
+++ Netty_Socket_Io_Server/src/main/java/com/firstsocketio/domain/MessageRepository.java
@@ -2,6 +2,7 @@ package com.firstsocketio.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,6 @@ import java.util.List;
 @Repository
 public interface MessageRepository extends JpaRepository<ChatMessage, Long> {
 
-    @Query(nativeQuery = true, value = "select * from chat_message m order by m.create_at desc limit 25")
-    List<ChatMessage> findChatMessages();
+    @Query(nativeQuery = true, value = "select * from chat_message m where m.room_num = :id order by m.create_at desc limit 25")
+    List<ChatMessage> findChatMessages(@Param("id") String id);
 }
