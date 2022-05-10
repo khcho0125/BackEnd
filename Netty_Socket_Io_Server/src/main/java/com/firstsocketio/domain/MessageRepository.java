714669f8cae61b4a37eff35ac8007552fa4b2163
cho kyeong hyun <khcho0125@icloud.com>
2022-05-10 17:05:03 +0900

    ⛏️ :: Message CRUD

--- Netty_Socket_Io_Server/src/main/java/com/firstsocketio/domain/MessageRepository.java
+++ Netty_Socket_Io_Server/src/main/java/com/firstsocketio/domain/MessageRepository.java
@@ -1,5 +1,6 @@
 package com.firstsocketio.domain;
 
+import com.firstsocketio.domain.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,6 +11,8 @@ import java.util.List;
 @Repository
 public interface MessageRepository extends JpaRepository<ChatMessage, Long> {
 
-    @Query(nativeQuery = true, value = "select * from chat_message m where m.room_num = :id order by m.create_at desc limit 25")
-    List<ChatMessage> findChatMessages(@Param("id") String id);
+    @Query(nativeQuery = true, value = "select * from chat_message m where m.room_code = :code order by m.create_at desc limit 25")
+    List<ChatMessage> findChatMessages(@Param("code") String roomCode);
+
+    void deleteByRoomCode(String roomCode);
 }
