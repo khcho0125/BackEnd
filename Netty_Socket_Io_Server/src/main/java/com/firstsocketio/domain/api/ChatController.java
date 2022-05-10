f98a78a2c33c2c83ec155fb9df40710e14546cac
cho kyeong hyun <khcho0125@icloud.com>
2022-05-10 17:06:40 +0900

    ⛏️ :: +Typing , +RoomJoin

---
+++ Netty_Socket_Io_Server/src/main/java/com/firstsocketio/domain/api/ChatController.java
@@ -0,0 +1,67 @@
+package com.firstsocketio.domain.api;
+
+import com.corundumstudio.socketio.SocketIONamespace;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.DataListener;
+import com.firstsocketio.domain.MessageRepository;
+import com.firstsocketio.domain.api.dto.EventMessage;
+import com.firstsocketio.domain.entity.ChatMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class ChatController {
+
+    private final SocketIONamespace namespace;
+    private final MessageRepository messageRepository;
+
+    @Autowired
+    public ChatController(SocketIOServer server, MessageRepository messageRepository) {
+        namespace = server.addNamespace("/chat");
+        namespace.addEventListener("send", ChatMessage.class, onMessage());
+        namespace.addEventListener("userJoin", EventMessage.class, onJoin());
+        namespace.addEventListener("userTyping", EventMessage.class, onTyping());
+        namespace.addEventListener("userStopTyping", EventMessage.class, onStopTyping());
+
+        this.messageRepository = messageRepository;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    private DataListener<ChatMessage> onMessage() {
+        return (client, data, ackSender) -> {
+            log.info("message: {}, user: {}, sessionId: {}", data.getMessage(), data.getUsername(), data.getSessionId());
+            namespace.getRoomOperations(data.getRoomCode()).sendEvent("newMessage", client, data);
+            messageRepository.save(data);
+        };
+    }
+
+    private DataListener<EventMessage> onJoin() {
+        return (client, data, ackSender) -> {
+            log.info("room:{}, user: {}, session: {}", data.getRoomCode(), data.getUsername(), data.getSessionId());
+            client.joinRoom(data.getRoomCode());
+            Integer online = namespace.getRoomOperations(data.getRoomCode()).getClients().size();
+            namespace.getRoomOperations(data.getRoomCode()).sendEvent("count", online);
+        };
+    }
+
+    private DataListener<EventMessage> onTyping() {
+        return (client, data, ackSender) -> {
+            log.info("Typing, session: {}", data.getSessionId());
+            namespace.getRoomOperations(data.getRoomCode()).sendEvent("userTyping", client, data);
+        };
+    }
+
+    private DataListener<EventMessage> onStopTyping() {
+        return (client, data, ackSender) -> {
+            log.info("StopTyping, session: {}", data.getSessionId());
+            namespace.getBroadcastOperations().sendEvent("userStopTyping", client, data);
+        };
+    }
+}
