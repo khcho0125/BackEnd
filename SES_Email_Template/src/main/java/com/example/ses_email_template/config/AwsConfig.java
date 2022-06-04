d0fa2c9d3a15da909c3b6c8eae0ff393f6ee9946
cho kyeong hyun <khcho0125@icloud.com>
2022-06-05 01:29:32 +0900

    ⚙️ :: Aws Config

---
+++ SES_Email_Template/src/main/java/com/example/ses_email_template/config/AwsConfig.java
@@ -0,0 +1,33 @@
+package com.example.ses_email_template.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsConfig {
+
+    @Value("${aws.ses.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.ses.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.ses.region}")
+    private String region;
+
+    @Bean
+    public AmazonSimpleEmailService amazonSimpleEmailService() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        AWSStaticCredentialsProvider awsStaticCredentialsProvider = new AWSStaticCredentialsProvider(basicAWSCredentials);
+
+        return AmazonSimpleEmailServiceClient.builder()
+                .withCredentials(awsStaticCredentialsProvider)
+                .withRegion(region)
+                .build();
+    }
+}
