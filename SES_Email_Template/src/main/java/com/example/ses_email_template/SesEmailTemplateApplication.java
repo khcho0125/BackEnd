fcc350c016643e96ce27911140d6416df0f72588
cho kyeong hyun <khcho0125@icloud.com>
2022-06-05 01:31:31 +0900

    🚩 :: 애플리케이션 러너

---
+++ SES_Email_Template/src/main/java/com/example/ses_email_template/SesEmailTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.ses_email_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesEmailTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SesEmailTemplateApplication.class, args);
+    }
+
+}
