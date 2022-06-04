e5a22e87b29fe13a6083c0a0dd0562e5f103cc20
cho kyeong hyun <khcho0125@icloud.com>
2022-06-05 01:31:42 +0900

    🚩 :: 애플리케이션 테스트

---
+++ SES_Email_Template/src/test/java/com/example/ses_email_template/SesEmailTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ses_email_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesEmailTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
